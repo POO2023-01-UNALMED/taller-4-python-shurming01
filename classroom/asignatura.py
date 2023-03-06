f9c4cd5d6f3154c21379f1d8d40a2910da3ded25
shurming01 <salashiguita.rs@gmail.com>
2023-03-05 19:42:28 -0500

    commin

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,34 @@
 class Asignatura:
-
-    def __init__(self, nombre, salon):
+ 
+    def __init__(self, nombre, salon ="remoto"): # Constructor
         self._nombre = nombre
         self._salon = salon
 
+    def __str__(self):
+        cadena = self._nombre +" "+self._salon
+        return cadena
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def get_Nombre(self):
+        return self._nombre
+
+    def set_Salon(self, salon):
+        self._salon = salon
+
+    def get_Salon(self):
+        return self._salon
+    
     # def __str__(self):
     #     pass
+
+if __name__ == "__main__":
+    asignatura = Asignatura()
+    print(asignatura)
+
+    
+   
+
+        
+
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,48 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = None
+    grado = "Grado 12" # None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo = "grupo predeterminado" , asignaturas = [], estudiantes = []): # Constructor /grupo ordinario
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):#kwargs
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    #def agregarAlumno(self, alumno, lista = []): #lista = []
+    #    #print(lista)
+    #    lista.append(alumno)
+    #    self.listadoAlumnos = self.listadoAlumnos + lista
+
+    def agregarAlumno(self, alumno, lista = None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
+        #print(lista)
         self.listadoAlumnos = self.listadoAlumnos + lista
+    
+    def __str__(self):
+        cadena = "Grupo de estudiantes: "+self._grupo
+        return cadena
 
     # def __str__(self):
     #     pass
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
+    def asignarNombre(cls, nombre="Grado 12"): #Grado 10
+        cls.grado = nombre
+   
+    @ classmethod
+    def asignarNombre(cls, nombre="Grado 4"): #Grado 6
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    def asignarNombre(cls, nombre="Grado 6"): #Grado 4
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+if __name__ == "__main__":
+    grupo = Grupo()
+    print(grupo)
